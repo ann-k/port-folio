eaf995afb475bdb2cf882689a4fb9783c2c44a9c
Anya Kovylyaeva <kovylyaeva.ann@gmail.com>
2020-04-27 21:59:40 +0300

    Changed js files architecture

---
+++ app/javascript/components/editorPortfolio.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
+
+import EditorJS from '@editorjs/editorjs'
+
+const Header = require('@editorjs/header')
+
+const editorPortfolio = document.addEventListener('DOMContentLoaded', () => {
+  let editorContainer = document.getElementById('editorPortfolio')
+
+  if (editorContainer) {
+    let dataFromBackend = JSON.parse(editorContainer.dataset.contents).content_data
+    let url = 'http://localhost:3000/' + editorContainer.dataset.url + '.json'
+
+    const editor = new EditorJS({
+      holder: 'editorPortfolio',
+      data: dataFromBackend,
+      onChange: () => {console.log('Now I know that Editor\'s content changed!')},
+      placeholder: 'Новое портфолио',
+      onChange: () => {
+        editor.save().then((outputData) => {
+          console.log(outputData);
+
+          fetch(url, {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({content: outputData})
+          })
+          .then(res => {
+            return res.json()
+          })
+          .then(data => console.log(data))
+          .catch((error) => {
+            console.error('Error:', error)
+          })
+        }).catch((error) => {
+          console.log('Saving failed: ', error)
+        })
+      },
+
+      tools: {
+        header: {
+          class: Header,
+          inlineToolbar: true,
+          config: {
+            placeholder: 'Enter a header',
+            levels: [1, 2, 3],
+            defaultLevel: 2
+          }
+        }
+      }
+    })
+  }
+})
+
+export default editorPortfolio
---
+++ app/javascript/components/editorProject.jsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
+
+import EditorJS from '@editorjs/editorjs'
+import ImageTool from '@editorjs/image'
+
+const Header = require('@editorjs/header')
+const List = require('@editorjs/list')
+const Embed = require('@editorjs/embed')
+
+const editorProject = document.addEventListener('DOMContentLoaded', () => {
+  // let projectName = document.getElementById('editorjstest').dataset.project
+  // let saveButton = document.getElementById('editorJsSaveButton')
+
+  let editorContainer = document.getElementById('editorProject')
+
+  if (editorContainer) {
+    let dataFromBackend = JSON.parse(document.getElementById('editorProject').dataset.contents).content_data
+    let url = 'http://localhost:3000/' + document.getElementById('editorProject').dataset.url + '.json'
+    console.log(url);
+
+    const editor = new EditorJS({
+      holder: 'editorProject',
+      data: dataFromBackend,
+      placeholder: 'Плейсхолдер',
+      onChange: () => {
+        editor.save().then((outputData) => {
+          console.log(outputData);
+
+          fetch(url, {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({content: outputData})
+          })
+          .then(res => {
+            return res.json()
+          })
+          .then(data => console.log(data))
+          .catch((error) => {
+            console.error('Error:', error)
+          })
+        }).catch((error) => {
+          console.log('Saving failed: ', error)
+        })
+      },
+
+      tools: {
+        header: {
+          class: Header,
+          inlineToolbar: true,
+          config: {
+            placeholder: 'Enter a header',
+            levels: [1, 2, 3],
+            defaultLevel: 2
+          }
+        },
+        list: {
+          class: List,
+          inlineToolbar: true,
+        },
+        embed: {
+          class: Embed,
+          inlineToolbar: true
+        },
+        image: {
+          class: ImageTool,
+          config: {
+            captionPlaceholder: 'Подпись',
+            endpoints: {
+              byFile: 'http://localhost:8008/uploadFile', // Your backend file uploader endpoint
+              // byUrl: 'http://localhost:8008/fetchUrl', // Your endpoint that provides uploading by Url
+            }
+          }
+        }
+      }
+    })
+  }
+})
+
+export default editorProject

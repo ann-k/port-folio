a4d316a82d96d4753ee3f7b7c81aa4eabacd813b
Anya Kovylyaeva <kovylyaeva.ann@gmail.com>
2020-05-28 23:31:02 +0300

    Deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -60,6 +60,10 @@ namespace :deploy do
         unless test("[ -f #{shared_path}/config/credentials/production.key ]")
           upload! 'config/credentials/production.key', "#{shared_path}/config/credentials/production.key"
         end
+
+        unless test("[ -f #{shared_path}/config/puma.rb ]")
+          upload! 'config/puma.rb', "#{shared_path}/config/puma.rb"
+        end
       end
     end
   end

5537233074254b9a32d70eb25228d500433d1048
Anya Kovylyaeva <kovylyaeva.ann@gmail.com>
2020-05-29 00:22:44 +0300

    Deploy

--- app/serializers/project_in_portfolio_serializer.rb
+++ app/serializers/project_in_portfolio_serializer.rb
@@ -1,3 +1,3 @@
-class ProjectInPortfolioSerializer < ActiveModel::Serializer do
+class ProjectInPortfolioSerializer < ActiveModel::Serializer
   attributes :id, :position, :portfolio_id, :project_id
 end
